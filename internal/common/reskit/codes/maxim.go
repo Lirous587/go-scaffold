75529bf9628d6ba14b47ef207f0cb26746b81204
Lirous587 <2115883273@qq.com>
2025-09-26 14:53:16 +0800

    feat:移除多余code

--- internal/common/reskit/codes/maxim.go
+++
@@ -1,6 +0,0 @@
-package codes
-
-// 格言相关错误 (2400-2599)
-var (
-	ErrMaximNotFound = ErrCode{Msg: "格言不存在", Type: ErrorTypeNotFound, Code: 2400}
-)
--- internal/common/reskit/codes/mock.go
+++
@@ -1,6 +0,0 @@
-package codes
-
-// Mock相关错误 (xx00-xx99)
-var (
-	ErrMockNotFound = ErrCode{Msg: "Mock不存在", Type: ErrorTypeNotFound, Code: 0000}
-)
